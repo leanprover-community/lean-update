5105caa337844b0ebca5bf557c8c5ceadc19766b
Seasawher <tellagoodstory@icloud.com>
2025-02-26 22:30:53 +0900

    get changed files

--- scripts/checkChanges.ps1
+++ scripts/checkChanges.ps1
@@ -1,9 +1,14 @@
-
 $leanToolchainDiff = git diff -w "lean-toolchain"
 $lakeManifestDiff = git diff -w "lake-manifest.json"
+$changedFiles = @()
 
-if ($leanToolchainDiff -or $lakeManifestDiff) {
+if ($leanToolchainDiff) { $changedFiles += "lean-toolchain" }
+if ($lakeManifestDiff) { $changedFiles += "lake-manifest.json" }
+
+if ($changedFiles.Count -gt 0) {
     Write-Output "files_changed=true"
+    Write-Output "changed_files=$( $changedFiles -join ', ' )"
 } else {
     Write-Output "files_changed=false"
+    Write-Output "changed_files="
 }
