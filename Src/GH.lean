cb7b8f4be08417ea32337ed884272106f41ca551
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-15 02:37:36 +0900

    add read/write `GITHUB_ENV`

--- Src/GH.lean
+++ Src/GH.lean
@@ -6,7 +6,11 @@ public import Src.Env
 utils for GitHub Action
 -/
 
-def outputFilePath : String := "output.txt"
+/-- this is used for local testing -/
+private def Local.GITHUB_OUTPUT : String := "output.txt"
+
+/-- this is used for local testing -/
+private def Local.GITHUB_ENV : String := "env.txt"
 
 /-- detect if the code is running in a GitHub Action environment -/
 def isRunningGHAction : IO Bool := do
@@ -15,13 +19,28 @@ def isRunningGHAction : IO Bool := do
   | .some "True" => pure true
   | _ => pure false
 
+def IO.FS.appendLineToFile (path : System.FilePath) (line : String) : IO Unit :=
+  IO.FS.withFile path IO.FS.Mode.append fun h => do
+    h.putStr s!"{line}\n"
+
 /-- write a key-value pair to the GitHub Actions output -/
 public def GH.writeOutput (key value : String) : IO Unit := do
   if (← isRunningGHAction) then
     let path ← IO.getEnv! "GITHUB_OUTPUT"
-    IO.FS.withFile path IO.FS.Mode.append fun h => do
-      h.putStr s!"{key}={value}\n"
+    IO.FS.appendLineToFile path s!"{key}={value}"
   else
     let line := s!"{key}={value}\n"
-    IO.FS.withFile outputFilePath IO.FS.Mode.append fun h => do
-      h.putStr line
+    IO.FS.appendLineToFile Local.GITHUB_OUTPUT line
+
+def GH.writeGHEnv (key value : String) : IO Unit := do
+  if (← isRunningGHAction) then
+    let path ← IO.getEnv! "GITHUB_ENV"
+    IO.FS.appendLineToFile path s!"LEAN_UPDATE_{key}={value}"
+  else
+    IO.FS.appendLineToFile Local.GITHUB_ENV s!"LEAN_UPDATE_{key}={value}"
+
+def GH.readGHEnv (key : String) : IO (Option String) := do
+  if (← isRunningGHAction) then
+    IO.getEnv s!"LEAN_UPDATE_{key}"
+  else
+    pure none
