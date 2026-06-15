b1a013759833a328aa9008b673bdd2fc4e685c8b
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-16 05:14:59 +0900

    bug fix: update argument pattern matching in main function

--- Main.lean
+++ Main.lean
@@ -5,6 +5,6 @@ import LeanUpdate.FindDep
 
 public def main (args : List String) : IO Unit := do
   match args with
-  | "findDependencies" :: [] => runFindDependencies
-  | "fetchLatest" :: rest => runFetchLatest rest
+  | ["findDependencies"] => runFindDependencies
+  | ["fetchLatest"] => runFetchLatest
   | _ => throw <| IO.userError "invalid arguments of leanUpdate"
