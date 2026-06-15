18b92419335cbbf2c82069df150602e7d0f56edd
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-16 03:32:26 +0900

    refactor: reorganize input handling by moving ActionInput to a new file and updating ReleaseKindToFetch

--- LeanUpdate/Input.lean
+++ LeanUpdate/Input.lean
@@ -3,7 +3,7 @@ module
 public meta import LeanUpdate.Deriving.ToString
 public meta import LeanUpdate.Deriving.HasParser
 public import LeanUpdate.HasParser
-public import LeanUpdate.GH
+public import LeanUpdate.ActionInput
 
 /-- The kind of Lean release -/
 public inductive ReleaseKindToFetch where
@@ -12,37 +12,6 @@ public inductive ReleaseKindToFetch where
   /-- nightly release -/
   | nightly
 deriving Repr, BEq, ToString, HasParser
-
-#guard toString ReleaseKindToFetch.tagged == "tagged"
-#guard toString ReleaseKindToFetch.nightly == "nightly"
-
-#guard
-  match (parseAs ReleaseKindToFetch "nightly") with
-  | .ok .nightly => true
-  | _ => false
-
-/-- the input value of GitHub Action -/
-public class ActionInput (α : Type) where
-  /-- the environment variable name which stores the input value -/
-  envName : String
-  /-- parse function for the input value -/
-  parse : String → Except String α
-  /-- default value for local testing -/
-  localValue? : Option α
-
-/-- get the input value from the environment variable or the local default value -/
-public def Input.get (α : Type) [ActionInput α] : IO α := do
-  let envVarName := ActionInput.envName α
-  match (← IO.getEnv envVarName) with
-  | .some valueStr =>
-    IO.ofExcept <| ActionInput.parse valueStr
-  | .none =>
-    if !(← GH.isRunningGHAction) then
-      match (ActionInput.localValue? : Option α) with
-      | some localVal => pure localVal
-      | none => throw <| IO.userError s!"Environment variable '{envVarName}' not found and no local default value provided."
-    else
-      throw <| IO.userError s!"Environment variable '{envVarName}' not found."
 
 public instance : ActionInput ReleaseKindToFetch where
   envName := "RELEASE_KIND_TO_FETCH"
