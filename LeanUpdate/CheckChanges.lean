57ec36d47ade55d877e955bd6889509ee7aff40c
Seasawher <47292598+Seasawher@users.noreply.github.com>
2026-06-16 23:44:53 +0900

    bug fix: empty `lean-toolchain` content is shown
    resolve #233
    
    This is caused by the environment variable `LATEST_LEAN` is only set when the lake project does not have dependencies.
    The action see empty `LATEST_LEAN` when the project has dependencies.

--- LeanUpdate/CheckChanges.lean
+++ LeanUpdate/CheckChanges.lean
@@ -5,15 +5,33 @@ import LeanUpdate.Input
 
 open IO Process System
 
-def filesToWatch : List String := ["lean-toolchain", "lake-manifest.json"]
+/-- the result of update command -/
+inductive UpdateResult where
+  /-- no changes detected. already up to date -/
+  | upToDate
+  /-- changes detected -/
+  | changed (newContent : String)
+
+def UpdateResult.isChanged : UpdateResult → Bool
+  | .upToDate => false
+  | .changed _ => true
+
+def UpdateResult.newContent : UpdateResult → String
+  | .upToDate => ""
+  | .changed content =>content
 
 structure CheckChangesResult where
-  filesChanged : Bool
-  doUpdate : Bool
-  changedFiles : String
-  leanToolchainUpdated : Bool
+  lakeManifest : UpdateResult
+  leanToolchain : UpdateResult
 
-def hasGitDiffIgnoringWhitespace (cwd : FilePath) (path : String) : IO Bool := do
+/-- Check if there are any diffs.
+
+### Note
+* This function ignores whitespace changes
+* This function uses `git diff` to check changes.
+* `path` is relative to the `cwd`
+-/
+def hasGitDiff (cwd : FilePath) (path : String) : IO Bool := do
   let out ← IO.Process.output {
     cmd := "git"
     args := #["diff", "-w", "--", path]
@@ -24,32 +42,48 @@ def hasGitDiffIgnoringWhitespace (cwd : FilePath) (path : String) : IO Bool := d
     throw <| IO.userError s!"Failed to check git diff for {path} with exit code {out.exitCode}{stderr}"
   pure !out.stdout.isEmpty
 
-def checkChanges (updateIfModified : UpdateIfModified) (targetLakePackageDir : FilePath) : IO CheckChangesResult := do
-  let changedFiles ← filesToWatch.filterM (hasGitDiffIgnoringWhitespace targetLakePackageDir)
-  let leanToolchainUpdated ← hasGitDiffIgnoringWhitespace targetLakePackageDir "lean-toolchain"
+def checkFile (targetLakePackageDir : FilePath) (file : FilePath) : IO UpdateResult := do
+  let hasDiff ← hasGitDiff targetLakePackageDir s!"{file}"
+  if hasDiff then
+    let content ← IO.FS.readFile (targetLakePackageDir / file)
+    pure <| .changed content.trimAscii.copy
+  else
+    pure .upToDate
 
-  let filesChanged := !changedFiles.isEmpty
-  let doUpdate :=
-    match updateIfModified with
-    | .«lean-toolchain» => leanToolchainUpdated
-    | .«lake-manifest.json» => filesChanged
-
-  pure {
-    filesChanged
-    doUpdate
-    changedFiles := String.intercalate " " changedFiles
-    leanToolchainUpdated
-  }
+def checkChanges (targetLakePackageDir : FilePath) : IO CheckChangesResult := do
+  let leanToolchainResult ← checkFile targetLakePackageDir "lean-toolchain"
+  let lakeManifestResult ← checkFile targetLakePackageDir "lake-manifest.json"
+  pure { lakeManifest := lakeManifestResult, leanToolchain := leanToolchainResult }
 
 /-- Run the update checker command. -/
 public def runCheckChanges : IO Unit := do
   let updateIfModified ← Input.get UpdateIfModified
   let targetLakePackageDir ← getTargetLakePackageDirectory
-  let result ← checkChanges updateIfModified targetLakePackageDir
+  let result ← checkChanges targetLakePackageDir
 
-  IO.println s!"info: files_changed={result.filesChanged}, do_update={result.doUpdate}, changed_files={result.changedFiles}, lean_toolchain_updated={result.leanToolchainUpdated}"
+  let filesChanged := [result.lakeManifest, result.leanToolchain].any UpdateResult.isChanged
+  let doUpdate :=
+    match updateIfModified with
+    | .«lean-toolchain» => result.leanToolchain.isChanged
+    | .«lake-manifest.json» =>
+      -- **TODO** oh no, this is a bit weird...
+      -- The input is "lake-manifest.json", but we will trigger update even if only `lean-toolchain` is changed.
+      -- I think this is not good and I should fix the misleading input structrue.
+      filesChanged
+  let changedFiles := [("lake-manifest.json", result.lakeManifest), ("lean-toolchain", result.leanToolchain)]
+    |>.filter (fun (_, res) => res.isChanged)
+    |>.map (fun (name, _) => name)
+  let leanToolchainUpdated := result.leanToolchain.isChanged
+  let newLeanToolchainContent := result.leanToolchain.newContent
+  IO.println s!"info: files_changed={filesChanged}, do_update={doUpdate}, changed_files={changedFiles}, lean_toolchain_updated={leanToolchainUpdated}"
 
-  GH.writeGHEnv "FILES_CHANGED" (toString result.filesChanged)
-  GH.writeGHEnv "CHANGED_FILES" result.changedFiles
-  GH.writeGHEnv "DO_UPDATE" (toString result.doUpdate)
-  GH.writeGHEnv "LEAN_TOOLCHAIN_UPDATED" (toString result.leanToolchainUpdated)
+  GH.writeGHEnv "FILES_CHANGED" (toString filesChanged)
+  GH.writeGHEnv "CHANGED_FILES" (String.intercalate " " changedFiles)
+  GH.writeGHEnv "DO_UPDATE" (toString doUpdate)
+  GH.writeGHEnv "LEAN_TOOLCHAIN_UPDATED" (toString leanToolchainUpdated)
+
+  if leanToolchainUpdated then
+    IO.println s!"info: new lean-toolchain content: {newLeanToolchainContent}"
+  else
+    IO.println s!"info: lean-toolchain not updated, no new content"
+  GH.writeGHEnv "NEW_LEAN_TOOLCHAIN_CONTENT" newLeanToolchainContent
